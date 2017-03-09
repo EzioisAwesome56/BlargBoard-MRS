230985c7ebf61875b95b259abe9ed026f46d2df2
EzioisAwesome56 <jedijon567@gmail.com>
2017-03-09 15:16:44 -0500

    give maor some credit, he needs it
    random emergency patch maker needs some of it

--- pages/credits.php
+++ pages/credits.php
@@ -7,7 +7,7 @@ div.Box { background: #4dabb3 !important; padding: 15px !important; margin: 25px
 Dirbaio, Kawa & co.: Made <a href="http://bit.ly/2bBpaD1">ABXD</a><br>
 Staplebutter: made <a href="http://bit.ly/2basjLN">Blargboard 1.2</a>, which is what this is based off of<br>
 EzioisAwesome56: Main coder of the MRS project<br>
-Maorninja: Coder of <a href="https://notabug.org/WorldWeb-XD/WorldWeb-XD">WorldWeb XD</a>, provided the IRC page among other things<br><br>
+Maorninja: Coder of <a href="https://notabug.org/WorldWeb-XD/WorldWeb-XD">WorldWeb XD</a>, provided the IRC page among other things. Also did an emergency patch<br><br>
 
 This is the final goodbye to the Blargboard MRS project. 1.2.5 is the last version of MRS to be made. Its been a fun ride, but im teaming up with xkitten and maorninja to make Memeboard<br>
 so look out for that<br>
